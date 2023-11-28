cf526661e8a5c1c760238acba9232864f4365930
Brayan13141 <s20120185@alumnos.itsur.edu.mx>
2023-11-28 09:04:39 -0600

    AVANCE METODO

---
+++ COMPILADOR-RESPALDO/COMPILADOR1/src/METODOS_GRAMATICA/VARIOS.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package METODOS_GRAMATICA;
+
+import GRAMATICA.TOKENS;
+import OBJETOS_TABLA.ERROR;
+import OBJETOS_TABLA.OBJETO_T;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author CC25
+ */
+public class VARIOS {
+
+    public ArrayList<ERROR> errores = new ArrayList<ERROR>();
+
+    public ArrayList<ERROR> VALIDARVARIOS(ArrayList<OBJETO_T> listaTokens) {
+        int count = 0;
+        TOKENS A = new TOKENS();//CREAMOS EL OBJETO DE TOKENS Y OBTENEMOS LOS TOKENS
+        String[][] TOK = A.getTOKENS(); // CREAMOS EL ARREGLO Y LO INICIALIZAMOS CON LOS TOKENS
+        ArrayList<OBJETO_T> LISTAT2 = new ArrayList<>();
+        for (OBJETO_T Cadena : listaTokens) {
+            LISTAT2.add(Cadena);
+            count++;
+            if (count == 5) {
+                break;
+            }
+        }
+        DECLARACION DECLARACION = new DECLARACION();
+        while (listaTokens.size()>1)//LOS ERRORES VERIFICAMOS CUANTOS SON Y EL QUE GENERE MENOS ES POR LA OPCION QUE ESTABA BIEN
+        {
+            if (DECLARACION.VALIDARDECLARACION(listaTokens).isEmpty()) {
+
+            }
+        }
+//        for (ERROR E : DECLARACION.VALIDARDECLARACION(listaTokens)) {
+//            errores.add(E);
+//        }
+
+        return errores;
+    }
+}
