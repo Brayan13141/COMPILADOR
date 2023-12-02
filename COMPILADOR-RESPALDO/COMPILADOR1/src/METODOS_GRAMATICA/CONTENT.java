bf38bb1dddf619ffab7b6b999df03a6031b2db97
Brayan13141 <s20120185@alumnos.itsur.edu.mx>
2023-12-02 16:47:10 -0600

    AVANCE-F

---
+++ COMPILADOR-RESPALDO/COMPILADOR1/src/METODOS_GRAMATICA/CONTENT.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package METODOS_GRAMATICA;
+
+import TOKENS.TOKENS;
+import OBJETOS_TABLA.OBJETO_T;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author CC25
+ */
+public class CONTENT {
+
+    public boolean VALIDARCUERPO(ArrayList<OBJETO_T> listaTokens) {
+        int count = 0;
+        TOKENS A = new TOKENS();//CREAMOS EL OBJETO DE TOKENS Y OBTENEMOS LOS TOKENS
+        String[][] TOK = A.getTOKENS(); // CREAMOS EL ARREGLO Y LO INICIALIZAMOS CON LOS TOKENS
+        ArrayList<OBJETO_T> LISTAT2 = new ArrayList<>();
+        for (OBJETO_T Cadena : listaTokens) {
+            LISTAT2.add(Cadena);
+            count++;
+            if (count == 5) {
+                break;
+            }
+        }
+     
+        return false;
+    }
+}
---
+++ COMPILADOR-RESPALDO/COMPILADOR1/src/OBJETOS_TABLA/OBJETO_T.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package OBJETOS_TABLA;
+
+/**
+ *
+ * @author BRAYAN 
+ */
+public class OBJETO_T {
+    String Linea;
+    String Lexema;
+    String TOKEN;
+
+    public OBJETO_T(String Linea, String Lexema, String TOKEN) {
+        this.Linea = Linea;
+        this.Lexema = Lexema;
+        this.TOKEN = TOKEN;
+    }
+
+    public String getLinea() {
+        return Linea;
+    }
+
+    public void setLinea(String Linea) {
+        this.Linea = Linea;
+    }
+
+
+    public String getLexema() {
+        return Lexema;
+    }
+
+    public void setLexema(String Lexema) {
+        this.Lexema = Lexema;
+    }
+
+    public String getTOKEN() {
+        return TOKEN;
+    }
+
+    public void setTOKEN(String TOKEN) {
+        this.TOKEN = TOKEN;
+    }
+    
+}
